4974a5e79de31b842696393f559fb0e91fc4dbd8
quekwk87 <111193587+quekwk87@users.noreply.github.com>
2023-01-30 18:43:03 +0800

    version 1

---
+++ jsApp.js
@@ -0,0 +1,36 @@
+// console.log('welcome to my JS')
+
+// let total = Math.random()
+
+// if (total === 4)
+// {
+//     console.log("not more")
+// }
+// else
+// {
+//     console.log(`after condition: ${total}`)
+// }
+
+// let userInput = prompt("enter your value")
+// if(userInput<100)
+// {
+//     console.log('less than 100')
+// }
+// else if (userInput<1000)
+// {
+//     console.log('between 500 to 1000')
+// }
+// else
+// {
+//     console.log('else')
+// }
+
+const password = prompt("please enter your pw")
+if(password.length>=6)
+{
+    console.log("long enough GOOD")
+}
+else
+{
+    console.log("longEr please!")
+}
